6db389546647a48500994c269c0da29b122f8dc3
Vasav Juyal <vasavj123@gmail.com>
2025-01-21 00:47:17 +0200

    Removed some columns and added some features. Will do GridSearch

--- funcs_ml.py
+++ funcs_ml.py
@@ -1,4 +1,7 @@
 from sklearn.metrics import classification_report
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
 def classify(model, X_train, y_train, X_test, y_test):
     model.fit(X_train, y_train)
@@ -6,4 +9,13 @@ def classify(model, X_train, y_train, X_test, y_test):
 
     report = classification_report(y_test, preds) 
     print(report)
-    
+
+def cv(model, X, y):
+    kf = KFold(n_splits = 5, shuffle=True, random_state=42)
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('model', model)
+    ])
+    score = cross_val_score(pipeline, X, y, cv=kf, scoring='accuracy')
+    print(f"Cross-validation scores: {score}")
+    print(f"Mean accuracy: {score.mean()}")
--- funcs_prep.py
+++ funcs_prep.py
@@ -5,49 +5,40 @@ import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
 import nltk 
-from nltk.tokenize import word_tokenize 
-from gensim.models import Word2Vec
+from nltk.tokenize import word_tokenize, sent_tokenize
 nltk.download('punkt')
 
 def clean_text(text, lemmatizer, stop_words): 
-    text = re.sub(r'at\s+\S+\.\S+\(.*?\)', '', text)
-    text = re.sub(r'http\S+|www\S+|https\S+', '',text, flags=re.MULTILINE)
-    text = '\n'.join([line for line in text.split('\n') if 'org.eclipse' not in line])
+    text = re.sub(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', '', text)
+    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
+    text = '\n'.join([line for line in text.split('\n') if 'org.eclipse' not in line]) 
+    text = re.sub(r'(\bat\b\s+\S+\.\S+\(.*?\))|(\bException\b.*?\n\s*at\s+.*?\(.*?\))|(\bError\b.*?\n\s*at\s+.*?\(.*?\))', '', text, flags=re.DOTALL) 
     text = re.sub(r'[^\w\s]', '', text) 
     text = text.lower()
-    tokens = word_tokenize(text)
+    tokens = word_tokenize(text) 
     text = ' '.join([lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]) 
     return text
     
-def combine_columns(data, lemmatizer, stop_words):
+def prepare_df(data, lemmatizer, stop_words):
     data['description'] = data['short_description']+' '+ data['long_description']
     data['description'] = data['description'].apply(lambda x: clean_text(x, lemmatizer, stop_words))
-
-    data['product'] = data['bug_id'] + ' '+ data['component_name'] + ' ' + data['product_name']
-    data['product'] = data['product'].apply(lambda x: clean_text(x, lemmatizer, stop_words))
+    data['word_count_desc'] = data['description'].apply(lambda row: len(word_tokenize(row)))
+    data['sent_count_desc'] = data['description'].apply(lambda row: len(sent_tokenize(row)))
+    
     return data
 
-def vectorizer(data, column, n_components):
-    tfidf = TfidfVectorizer(max_features=5000, ngram_range=(1, 2))
+def vectorizer(data, column, n_components, features):
+    tfidf = TfidfVectorizer(max_features=features, ngram_range=(1, 2))
     tfidf_matrix = tfidf.fit_transform(data[column]).toarray()
     pca = PCA(n_components=n_components)
     pca_matrix = pca.fit_transform(tfidf_matrix) 
     df = pd.DataFrame(pca_matrix, columns=[f'PC{i+1}' for i in range(n_components)]) 
     return df
 
-def train_word2vec(data, column): 
-    tokenized_data = data[column].tolist() 
-    model = Word2Vec(sentences=tokenized_data, vector_size=100, window=5, min_count=1, workers=4) 
-    return model 
-def get_word2vec_vectors(tokens, model): 
-    vectors = [model.wv[word] for word in tokens if word in model.wv] 
-    return np.mean(vectors, axis=0) if vectors else np.zeros(model.vector_size)
-
-def processed_df(df1, df2, df3):
+def processed_df(df1, df2):
     df1.reset_index(drop=True, inplace=True) 
     df2.reset_index(drop=True, inplace=True) 
-    df3.reset_index(drop=True, inplace=True)
 
-    df_combined = pd.concat([df1, df2, df3], axis=1)
-    df_combined = df_combined.drop(columns=['bug_id', 'component_name','product_name', 'short_description', 'long_description', 'description', 'product'])
+    df_combined = pd.concat([df1, df2], axis=1)
+    df_combined = df_combined.drop(columns=['bug_id', 'component_name','product_name', 'short_description', 'long_description', 'description'])
     return df_combined
