995e6a18013179d156ae4e3e4363196c2ae8c11c
Vasav Juyal <vasavj123@gmail.com>
2025-01-27 17:31:54 +0200

    Add data, final paramater change and change variable names

--- funcs_prep.py
+++ funcs_prep.py
@@ -12,7 +12,7 @@ nltk.download('punkt')
 def clean_text(text, lemmatizer, stop_words): 
     text = re.sub(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', '', text)
     text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
-    text = '\n'.join([line for line in text.split('\n') if 'org.eclipse' not in line]) 
+    # text = '\n'.join([line for line in text.split('\n') if 'org.eclipse' not in line]) 
     # text = re.sub(r'(\bat\b\s+\S+\.\S+\(.*?\))|(\bException\b.*?\n\s*at\s+.*?\(.*?\))|(\bError\b.*?\n\s*at\s+.*?\(.*?\))', '', text, flags=re.DOTALL) 
     text = re.sub(r'[^\w\s]', '', text) 
     text = text.lower()
@@ -23,8 +23,6 @@ def clean_text(text, lemmatizer, stop_words):
 def prepare_df(data, lemmatizer, stop_words):
     data['description'] = data['short_description']+' '+ data['long_description']
     data['description'] = data['description'].apply(lambda x: clean_text(x, lemmatizer, stop_words))
-    data['word_count_desc'] = data['description'].apply(lambda row: len(word_tokenize(row)))
-    # data['char_count_desc'] = data['description'].apply(lambda row: len(row) )
     data = data.drop(columns=['short_description', 'long_description']) 
     return data
 
@@ -43,7 +41,7 @@ def processed_df(df1, df2):
     df_combined = df_combined.drop(columns=['description'])
     return df_combined
 
-def w2v(data,vector_size=100, window=5, min_count=1, workers=4):
+def w2v(data,vector_size=100, window=5, min_count=5, workers=4):
     sentences = data['tokens'].tolist()
     model = Word2Vec(sentences, vector_size=vector_size, window=window, min_count=min_count, workers=workers)
     return model
